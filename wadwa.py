dd4bede5520941a5a87651e79cd969a56fe2ee32
Adi <ripedown1@gmail.com>
2025-10-08 15:14:50 +0530

    add python file

--- wadwa.py
+++ wadwa.py
@@ -4,3 +4,11 @@ print("Hello world")
 def fun():
     print("ASOMETHI")
     a = {"wad": "Wad"}
+
+
+def av():
+    print('something')
+    b = {
+        'ad':'wad', 'aw':'w',
+        'wada': 'wadwad'
+    }
